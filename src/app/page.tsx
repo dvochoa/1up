df00a873022a5bc5edc5b010eb5e8f3723712a99
Danny Ochoa <danny@dannyochoa.net>
2024-12-23 18:07:18 -0500

    Add initial timer UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import Timer from "@/components/timer/timer";
+
 export default function HomePage() {
   return (
     <div>
       <meta charSet="utf-8"></meta>
       <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       <main>
-        <div>Hello World</div>
+        <Timer></Timer>
       </main>
     </div>
   );
