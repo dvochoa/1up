c71a65474923f4bad4e93233eac3b13d16f6f473
Danny Ochoa <danny@dannyochoa.net>
2025-01-31 22:49:27 -0500

    fixup

--- backend/main.go
+++ backend/main.go
@@ -10,16 +10,6 @@ import (
 func main() {
 	router := gin.Default()
 
-	// CORS Configuration
-	// config := cors.DefaultConfig()
-	// config.AllowOrigins = []string{"http://localhost:3000"}
-	// config.AllowHeaders = []string{"Origin", "Content-Type", "Accept", "Authorization"}
-	// config.ExposeHeaders = []string{"Content-Length"}
-	// config.AllowCredentials = true
-
-	// Apply CORS middleware
-	// router.Use(cors.New(config))
-
 	// Specify routes
 	router.GET("/timers", handlers.TimersHandler)
 
