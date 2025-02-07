22267493292af04474f8434504ac90f44a524b4b
Danny Ochoa <danny@dannyochoa.net>
2025-02-06 22:58:02 -0500

    Set up hot reloading with Docker

---
+++ client/next.config.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+      };
+    }
+    return config;
+  },
+};
+
+export default nextConfig;
