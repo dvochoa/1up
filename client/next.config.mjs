c78636bf7e02ee936cda0ce2e9972907ad7af332
Danny Ochoa <danny@dannyochoa.net>
2025-04-25 23:52:47 -0400

    Improve dev setup Misc changes

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -5,15 +5,6 @@
  */
 const nextConfig = {
   output: "standalone",
-  webpack: (config, { dev }) => {
-    if (dev) {
-      config.watchOptions = {
-        poll: 1000,
-        aggregateTimeout: 300,
-      };
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
